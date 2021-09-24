d0245f94bc2554134c57c1d5257ba4c6c2d1f824
ayahmuhamed <88331884+ayahmuhamed@users.noreply.github.com>
2021-09-24 12:52:54 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+variable "ayatest" {
+  type    = string
+  default = ""
+}
+
+variable "ayatest1" {
+  type    = string
+  default = ""
+}
+
+
+output "ayatest" {
+  value = var.ayatest
+}
+
+output "ayatest1" {
+  value = var.ayatest1
+}
